f7d6bd5c8d1fe4fedfee950271bac79308c5d0a6
Amanda Morris <amandafmorris@gmail.com>
2017-04-09 20:30:29 +0000

    ll-delete-node done

--- ll-delete-node.py
+++ ll-delete-node.py
@@ -12,4 +12,20 @@
 """
 
 def delete(head, position):
-    
+    """Deletes the positionth item from a linkedlist, and returns the head"""
+    # to delete the head:
+    if position == 0:
+        new_head = head.next
+        head.next = None
+        return new_head
+
+    #  to delete a middle node:
+    current = head
+    index = 1  # initialize index to correspond to current.next
+    while current.next:
+        if index == position:
+            current.next = current.next.next
+            return head
+        else:  # advance index and current node
+            index += 1
+            current = current.next
