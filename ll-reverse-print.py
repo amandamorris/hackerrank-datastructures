935b6033369af855f6661fc4fa5bb600903b9896
Amanda Morris <amandafmorris@gmail.com>
2017-04-09 20:36:05 +0000

    ll-reverse-print done

--- ll-reverse-print.py
+++ ll-reverse-print.py
@@ -13,6 +13,20 @@
 """
 
 def ReversePrint(head):
+    """Prints out a linked list in reverse.
+    Idea: walk through list, adding each node's data to a new list.
+    Reverse-print elements in the list.
+    """
+
+    ll_data = []  # empty list to store node data values
+
+    current = head
+    while current:
+        ll_data.append(current.data)
+        current = current.next
+
+    for data in ll_data[::-1]:
+        print data
 
 
   
